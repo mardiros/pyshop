1447680772c09f03e25092151ab273eac31bf249
Guillaume Gauvrit <ggauvrit@laposte.net>
2012-12-27 23:36:34 +0100

    Reset version number

--- pyshop/__init__.py
+++ pyshop/__init__.py
@@ -8,7 +8,7 @@ from .models import create_engine
 from .helpers.i18n import locale_negotiator
 from .helpers.authentication import RouteSwithchAuthPolicy
 
-__version__ = '0.3'
+__version__ = '0.4-dev'
 
 
 def main(global_config, **settings):
