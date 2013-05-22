f953c6859e3fe351fd9ae22cc34c9e9e97697dbb
Guillaume Gauvrit <ggauvrit@laposte.net>
2013-05-22 22:25:37 +0200

    Bump new development version

--- pyshop/__init__.py
+++ pyshop/__init__.py
@@ -13,7 +13,7 @@ from .models import create_engine
 from .helpers.i18n import locale_negotiator
 from .helpers.authentication import RouteSwithchAuthPolicy
 
-__version__ = '0.7.6'
+__version__ = '0.7.7~dev'
 
 
 def main(global_config, **settings):
