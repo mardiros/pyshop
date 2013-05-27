4dd49a050ab64cd30cc2a78beb3edff4b41b7ed4
Guillaume Gauvrit <ggauvrit@laposte.net>
2013-05-27 22:47:20 +0200

    Add docstring and prefer str.format rather that %

--- pyshop/helpers/download.py
+++ pyshop/helpers/download.py
@@ -1,3 +1,13 @@
+#-*- coding: utf-8 -*-
+"""
+Pyramid helper that mirror and serve file automatically from PyPI.
+
+Package are mirrored safety by validating the PyPI certificate.
+
+It is possible to use an HTTP Proxy. Pyshop use the requests library, so,
+you just have to export your proxy in the environment variable `https_proxy`.
+"""
+
 import os
 import os.path
 import mimetypes
@@ -14,6 +24,8 @@ mimetypes.add_type('x-application/egg', '.egg')
 
 
 class ReleaseFileRenderer(object):
+    """Renderer that serve the python package"""
+
     implements(ITemplateRenderer)
 
     def __init__(self, repository_root):
@@ -42,10 +54,10 @@ class ReleaseFileRenderer(object):
 
                 verify = value['url'].startswith('https:')
 
-                log.info('downloading %s', value['url'])
+                log.info('Downloading {0}'.format(value['url']))
                 resp = requests.get(value['url'], verify=verify)
                 if resp.status_code == 404:
-                    raise NotFound('Resource {} not found'.format(filename))
+                    raise NotFound('Resource {0} not found'.format(filename))
                 resp.raise_for_status()
                 with open(localpath, 'wb') as file:
                     file.write(resp.content)
@@ -63,4 +75,9 @@ class ReleaseFileRenderer(object):
 
 
 def renderer_factory(info):
+    """
+    Create the :class:`pyshop.helpers.download.ReleaseFileRenderer` factory.
+    Packages are stored in the directory in the configuration key
+    ``pyshop.repository`` of the paste ini file.
+    """
     return ReleaseFileRenderer(info.settings['pyshop.repository'])
--- pyshop/helpers/pypi.py
+++ pyshop/helpers/pypi.py
@@ -1,3 +1,25 @@
+"""
+PyPI XML-RPC Client helper
+
+To use the :data:`pyshop.helpers.pypi.proxy`, the method
+:method:`pyshop.helpers.pypi.set_proxy`must be called to
+install the XML RPC proxy.
+
+Also it is possible to install an HTTP Proxy in case the pyshop host does not
+have a direct access to internet.
+
+This is actually a configuration key ``pyshop.pypi.transport_proxy`` from the
+paste ini file.
+
+.. warning::
+    This code use an unsecured connection (http)
+    and should be modified since PyPI is available in https.
+
+.. :data:`proxy`:: The XML RPC Proxy
+
+
+"""
+
 import httplib
 from xmlrpclib import ServerProxy, Transport
 
@@ -23,8 +45,8 @@ class ProxiedTransport(Transport):
     connection.putheader('Host', self.realhost)
 
 
-
 def set_proxy(proxy_url, transport_proxy=None):
+    """Create the proxy to PyPI XML-RPC Server"""
     global proxy
     transport = ProxiedTransport(transport_proxy) if transport_proxy else None
     proxy = ServerProxy(proxy_url, transport=transport, allow_none=True)
