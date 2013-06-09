647fa0b6b848d2448fb4dd7f7dbcdb316b319096
Guillaume Gauvrit <guillaume@gauvr.it>
2013-06-09 23:12:05 +0200

    Release 0.8

--- pyshop/__init__.py
+++ pyshop/__init__.py
@@ -13,7 +13,7 @@ from .models import create_engine
 from .helpers.i18n import locale_negotiator
 from .helpers.authentication import RouteSwithchAuthPolicy
 
-__version__ = '0.7.7~dev'
+__version__ = '0.8'
 
 
 def main(global_config, **settings):
