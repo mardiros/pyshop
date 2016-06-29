d6f1f83c8d4c32a98c0bf050c1183315902190c2
Last G <last_g@hackerdom.ru>
2016-06-29 11:13:57 +0200

    Misstype with error handling

--- pyshop/helpers/authentication.py
+++ pyshop/helpers/authentication.py
@@ -25,7 +25,7 @@ class AuthBasicAuthenticationPolicy(CallbackAuthenticationPolicy):
         auth = request.environ.get('HTTP_AUTHORIZATION')
         try:
             authmeth, auth = auth.split(' ', 1)
-        except AttributeError as ValueError:  # not enough values to unpack
+        except AttributeError:  # not enough values to unpack
             return None
 
         if authmeth.lower() != 'basic':
