dff1f0c812757469c9d03340fab51cd5d9b9c0d8
Guillaume Gauvrit <guillaume@gauvr.it>
2017-06-29 20:17:02 +0200

    Replace pyramid_xmlrpc by pyramid_rpc to handle xmlrpc

--- pyshop/tests/case.py
+++ pyshop/tests/case.py
@@ -24,6 +24,7 @@ class ModelTestCase(TestCase):
     def tearDown(self):
         transaction.commit()
 
+
 class DummyRoute(object):
     name = 'index'
 
@@ -45,6 +46,7 @@ class UnauthenticatedViewTestCase(TestCase):
         self.maxDiff = None
         authz_policy = ACLAuthorizationPolicy()
         self.config = testing.setUp(settings=settings)
+        self.config.include('pyramid_rpc.xmlrpc')
         self.config.include(includeme)
         self.session = DBSession()
         transaction.begin()
@@ -56,8 +58,8 @@ class UnauthenticatedViewTestCase(TestCase):
         testing.tearDown()
 
     def create_request(self, params=None, environ=None, matchdict=None,
-                headers=None,
-                path='/', cookies=None, post=None, **kw):
+                       headers=None,
+                       path='/', cookies=None, post=None, **kw):
         if params and not isinstance(params, MultiDict):
             mparams = MultiDict()
             for k, v in params.items():
@@ -66,8 +68,8 @@ class UnauthenticatedViewTestCase(TestCase):
                 else:
                     mparams.add(k, v)
                 params = mparams
-        rv  = DummyRequest(params, environ, headers, path, cookies,
-                post, matchdict=(matchdict or {}), **kw)
+        rv = DummyRequest(params, environ, headers, path, cookies,
+                          post, matchdict=(matchdict or {}), **kw)
         return rv
 
     def assertIsRedirect(self, view):
